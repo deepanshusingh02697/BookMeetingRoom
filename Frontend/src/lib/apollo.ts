75668fbdba4b46fc88581eff6d7b617493315f67
Deepanshu Singh <deepanshu.s@celestialsys.com>
2026-08-18 12:13:16 +0530

    deploy frontend

--- Frontend/src/lib/apollo.ts
+++ Frontend/src/lib/apollo.ts
@@ -2,6 +2,7 @@ import { ApolloClient, HttpLink } from "@apollo/client";
 import { InMemoryCache } from "@apollo/client";
 
 const httpLink = new HttpLink({
+  // uri:"https://localhost:4002/graphql",
   uri: "https://bookmeetingroom-73rz.onrender.com/graphql",
   credentials: "include",
 });
